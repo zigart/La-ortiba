651d0219a9f6dc51c7819c45cbade7bdf2ae4ee3
zigart <agustinzigart@gmail.com>
2021-02-07 16:16:50 -0300

    agregado del contenedor con descripcion y segundo parallax con imagenes de prueba

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,14 +4,28 @@ var controller = new ScrollMagic.Controller();
 
 
 var parallaxT1 = new TimelineMax();
-parallaxT1.from('.bcg', 1, { y: '-50%', ease: Power0.easeNone }, 0);
+parallaxT1.from('.bcg1', 1, { y: '-50%', ease: Power0.easeNone }, 0);
+
+var parallaxT2 = new TimelineMax();
+parallaxT2.from('.bcg2', 1, { y: '-50%', ease: Power0.easeNone }, 0);
+
 
 //escenas
 var scene1 = new ScrollMagic.Scene({
-    triggerElement: '.bcg-parallax',
-    duration: '140%',
+    triggerElement: '.parallaxQuienesSomos',
+    duration: '190%',
     triggerHook: 0.9
 })
 
 .setTween(parallaxT1)
+    .addIndicators()
+    .addTo(controller)
+
+
+var scene2 = new ScrollMagic.Scene({
+        triggerElement: '.parallaxIntegrantes',
+        duration: '140%',
+        triggerHook: 0.9
+    }).setTween(parallaxT2)
+    .addIndicators()
     .addTo(controller)

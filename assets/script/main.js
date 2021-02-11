9f8995570860f6fdd7d411c743ee74b0b1c5b8a1
zigart <agustinzigart@gmail.com>
2021-02-11 17:34:00 -0300

    Se decidio eliminar el parallax en version smartphone por problemas de rendimiento

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,51 +1,57 @@
-//scroll
-var controller = new ScrollMagic.Controller();
-//tiempo
+var anchoVentana = $(window).width();
 
 
-var parallaxT1 = new TimelineMax();
-parallaxT1.from('.bcg1', 1, { y: '-50%', ease: Power0.easeNone }, 0);
+if (anchoVentana > 768) {
+    //scroll
+    var controller = new ScrollMagic.Controller();
+    //tiempo
+    var parallaxT1 = new TimelineMax();
+    parallaxT1.from('.bcg1', 1, { y: '-50%', ease: Power0.easeNone }, 0);
 
-var parallaxT2 = new TimelineMax();
-parallaxT2.from('.bcg2', 1, { y: '-50%', ease: Power0.easeNone }, 0);
+    var parallaxT2 = new TimelineMax();
+    parallaxT2.from('.bcg2', 1, { y: '-50%', ease: Power0.easeNone }, 0);
 
-var parallaxT3 = new TimelineMax();
-parallaxT3.from('.bcg3', 1, { y: '-50%', ease: Power0.easeNone }, 0);
+    var parallaxT3 = new TimelineMax();
+    parallaxT3.from('.bcg3', 1, { y: '-50%', ease: Power0.easeNone }, 0);
 
-var parallaxT4 = new TimelineMax();
-parallaxT4.from('.bcg4', 1, { y: '-50%', ease: Power0.easeNone }, 0);
+    var parallaxT4 = new TimelineMax();
+    parallaxT4.from('.bcg4', 1, { y: '-50%', ease: Power0.easeNone }, 0);
 
-//escenas
-var scene1 = new ScrollMagic.Scene({
-    triggerElement: '.parallaxQuienesSomos',
-    duration: '190%',
-    triggerHook: 1
-})
+    //escenas
+    var scene1 = new ScrollMagic.Scene({
+        triggerElement: '.parallaxQuienesSomos',
+        duration: '190%',
+        triggerHook: 1
+    })
 
-.setTween(parallaxT1)
-    .addTo(controller)
+    .setTween(parallaxT1)
+        .addTo(controller)
 
 
-var scene2 = new ScrollMagic.Scene({
-        triggerElement: '.parallaxIntegrantes',
-        duration: '170%',
-        triggerHook: 2
-    }).setTween(parallaxT2)
-    .addTo(controller)
+    var scene2 = new ScrollMagic.Scene({
+            triggerElement: '.parallaxIntegrantes',
+            duration: '170%',
+            triggerHook: 2
+        }).setTween(parallaxT2)
+        .addTo(controller)
 
-var scene3 = new ScrollMagic.Scene({
-        triggerElement: '.parallaxShows',
-        duration: '170%',
-        triggerHook: 2
-    }).setTween(parallaxT3)
-    .addTo(controller)
+    var scene3 = new ScrollMagic.Scene({
+            triggerElement: '.parallaxShows',
+            duration: '170%',
+            triggerHook: 2
+        }).setTween(parallaxT3)
+        .addTo(controller)
 
-var scene4 = new ScrollMagic.Scene({
-        triggerElement: '.parallaxContacto',
-        duration: '134%',
-        triggerHook: 2
-    }).setTween(parallaxT4)
-    .addTo(controller)
+    var scene4 = new ScrollMagic.Scene({
+            triggerElement: '.parallaxContacto',
+            duration: '134%',
+            triggerHook: 2
+        }).setTween(parallaxT4)
+        .addTo(controller)
+}
+
+
+
 
 $(document).ready(function() {
 

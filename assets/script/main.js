e6b66579a43a3b6035827ee79069b2e2584600bc
zigart <agustinzigart@gmail.com>
2021-09-05 22:03:07 -0300

    Se utilizo un patron diferente para el scroll parallax eliminando la libreria utilizada anteriormente

--- assets/script/main.js
+++ assets/script/main.js
@@ -58,99 +58,39 @@ $(document).ready(function() {
     }
 
 
+        // for (let i = 0; i <= 4; i++) {
+        //     $(".bcg" + i).css("height", "190%");
+        // }
 
-
-
-
-    //Busca en el navegador la version, windows, linux o mac
-
-    var navInfo = window.navigator.appVersion.toLowerCase();
-    var so = 'Sistema Operativo';
-
-    //Mete en una variable el valor de Navinfo dependiendo lo que encuentre
-    function retornarSO() {
-        if (navInfo.indexOf('win') != -1) {
-            so = 'Windows';
-        } else if (navInfo.indexOf('linux') != -1) {
-            so = 'Linux';
-        } else if (navInfo.indexOf('mac') != -1) {
-            so = 'Macintosh';
+        function react(image){
+            let getYposition = image.getBoundingClientRect().y;
+            return getYposition - window.innerHeight;
         }
-        return so;
-    }
+        const bcg1 = document.querySelector('.bcg1');
+        const bcg2 = document.querySelector('.bcg2');
+        const bcg3 = document.querySelector('.bcg3');
+        const bcg4 = document.querySelector('.bcg4');
 
-
-
-
-    //inicializa la funcion
-    retornarSO();
-
-
-    //Si en el 'so' se encontro alguno de estos dos valores ejecuta lo siguiente
-    if (so == "Windows" || so == "Macintosh") {
-
-        for (let i = 0; i <= 4; i++) {
-            $(".bcg" + i).css("height", "190%");
+        function scrollParallax(bcg){
+            let scrollBcg = bcg.getBoundingClientRect().y - window.innerHeight;
+            bcg.style.transform = "translateY("+  scrollBcg * 0.2+ "px)";
         }
 
-        //Se inicia la libreria
-        var controller = new ScrollMagic.Controller();
-
-        //Se define los objetos del dom que utilizaran el efecto
-
-        var propiedadesParallax = { y: '-50%', ease: Power0.easeNone }
-
-        var parallaxT1 = new TimelineMax();
-        parallaxT1.from('.bcg1', 1, propiedadesParallax, 0);
-
-        var parallaxT2 = new TimelineMax();
-        parallaxT2.from('.bcg2', 1, propiedadesParallax, 0);
-
-        var parallaxT3 = new TimelineMax();
-        parallaxT3.from('.bcg3', 1, propiedadesParallax, 0);
-
-        var parallaxT4 = new TimelineMax();
-        parallaxT4.from('.bcg4', 1, propiedadesParallax, 0);
-
-        //Propiedades del efecto parallax
-
-        var scene1 = new ScrollMagic.Scene({
-                triggerElement: '.parallaxQuienesSomos',
-                duration: '190%',
-                triggerHook: 1
-            })
-            .setTween(parallaxT1)
-            .addTo(controller);
-
-
-        var scene2 = new ScrollMagic.Scene({
-                triggerElement: '.parallaxIntegrantes',
-                duration: '170%',
-                triggerHook: 2
-            }).setTween(parallaxT2)
-            .addTo(controller);
-
-
-        var scene3 = new ScrollMagic.Scene({
-                triggerElement: '.parallaxShows',
-                duration: '170%',
-                triggerHook: 2
-            }).setTween(parallaxT3)
-            .addTo(controller);
-
-
-        var scene4 = new ScrollMagic.Scene({
-                triggerElement: '.parallaxContacto',
-                duration: '134%',
-                triggerHook: 2
-            }).setTween(parallaxT4)
-            .addTo(controller);
-
+        window.onscroll = function(){
+            if(react(bcg1) < 0 || react(bcg1) === 0){
+                scrollParallax(bcg1);
+            }
+            if(react(bcg2) < 0 || react(bcg2) === 0){
+                scrollParallax(bcg2);
+            }
+            if(react(bcg3) < 0 || react(bcg3) === 0){
+                scrollParallax(bcg3);
+            }
+            if(react(bcg4) < 0 || react(bcg4) === 0){
+                scrollParallax(bcg4);
+            }
+        }
         menu();
-    } else {
-        menu();
-    }
-
 
 
     //Propiedades del carrousel de descripciones
